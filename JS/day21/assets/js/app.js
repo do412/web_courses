d2feefa95a6167f7dc7d5ff0942e66779b5722eb
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-11-29 23:01:41 +0200

    Add files via upload

---
+++ JS/day21/assets/js/app.js
@@ -0,0 +1,33 @@
+// alert("Hello Everyone!")
+// document.write("Hi")
+
+// var - let - const
+var num = 6; // number
+var text = "myName"; // string
+var isExist = true; // boolean
+var primes = [1, 2, 3, 5] // array
+var player = {
+  height: 200,
+  strength: 90,
+  nationality: "Egyptian"
+} // object
+var dummy; // undefined
+dummy = true;
+
+console.log(num) // 6
+
+num = 8 + 2;
+console.log(num) // 10
+num = "x";
+console.log(num) // x
+
+console.log(typeof dummy) // undefined
+console.log(dummy) // undefined
+
+
+let x = 3;
+console.log(x)
+// let x = 9;
+// console.log(x)
+x = 9
+console.log(x)
